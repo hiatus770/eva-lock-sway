d266d64ef08b109a63691afc8382e83a8d13bc16
Matias <77402029+hiatus770@users.noreply.github.com>
2025-05-24 23:55:18 -0400

    Fixed weird resoultion shfiting issue

--- src/shaders/gaussian.fs
+++ src/shaders/gaussian.fs
@@ -10,6 +10,7 @@ uniform float weight[5] = float[] (0.227027, 0.1945946, 0.1216216, 0.054054, 0.0
 
 void main()
 {             
+    // vec2 tempSize = vec2(1920, 1080); 
     vec2 tex_offset = 1.0 / textureSize(image, 0); // gets size of single texel
     vec3 result = texture(image, TexCoords).rgb * weight[0]; // current fragment's contribution
     if(horizontal)
