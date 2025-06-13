07642f21f2396cc147a6943f5324f831654a26fd
Matias <77402029+hiatus770@users.noreply.github.com>
2025-06-13 19:21:13 -0400

    feat added pointer events

--- include/client_state.h
+++ include/client_state.h
@@ -4,31 +4,36 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <EGL/egl.h>
+#include "wayland/pointer.h"
 #include "globals.h"
 
 struct client_state
 {
-    // Wayland specific 
+    // Wayland specific
     struct wl_display *wl_display;
     struct wl_compositor *compositor;
     struct wl_shm *wl_shm;
     struct wl_registry *wl_registry;
     struct xdg_wm_base *xdg_wm_base;
-    struct wl_seat *wl_seat; 
+    struct wl_seat *wl_seat;
 
-    // Surface related wayland things 
+    // Surface related wayland things
     struct wl_surface *wl_surface;
     struct xdg_surface *xdg_surface;
     struct xdg_toplevel *xdg_toplevel;
-    
-    // Input related wayland things 
-    struct wl_keyboard *wl_keyboard; 
-    struct wl_pointer *wl_pointer; 
-    struct wl_touch *wl_touch; 
+
+    // Input related wayland things
+    struct wl_keyboard *wl_keyboard;
+    struct wl_pointer *wl_pointer;
+    struct wl_touch *wl_touch;
+
+    // For pointer
+    struct pointer_event pointer_event;
+
 
     int width, height;
     bool closed;
-    clock_state state; 
+    clock_state state;
 
     // State
     float offset;
--- include/wayland/wl_seat.h
+++ include/wayland/wl_seat.h
@@ -9,6 +9,15 @@
 
 static void wl_seat_capabilities(void* data, struct wl_seat *wl_seat, uint32_t capabilities){
     struct client_state *state = data;
+    
+    bool have_pointer = capabilities & WL_SEAT_CAPABILITY_POINTER; 
+    if (have_pointer && state->wl_pointer == NULL){
+        state->wl_pointer = wl_seat_get_pointer(state->wl_seat); 
+        wl_pointer_add_listener(state->wl_pointer, &wl_pointer_listener, state); 
+    } else if (!have_pointer && state->wl_pointer != NULL){
+        wl_pointer_release(state->wl_pointer); 
+        state->wl_pointer = NULL; 
+    }
 }
 
 // Just prints the name for now
